192604e8231381ee0e014232dc9537bcbf229515
Jeff Haynie <jhaynie@pinpoint.com>
2020-06-04 22:35:13 -0500

    changes for running in the new way

--- integration.go
+++ integration.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/pinpt/agent.next.github/internal"
+import (
+	"github.com/pinpt/agent.next.github/internal"
+	"github.com/pinpt/agent.next/runner"
+)
 
 // Integration is used to export the integration
 var Integration internal.GithubIntegration
 
 func main() {
+	runner.Main(&Integration)
 }
