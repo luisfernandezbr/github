58d243609385107672df84cfc1193b492ccf2d2a
Robindiddams <rdiddams@pinpoint.com>
2020-08-27 14:29:17 -0500

    use name as id 😬

--- internal/autoconfig.go
+++ internal/autoconfig.go
@@ -22,7 +22,7 @@ func (g *GithubIntegration) AutoConfigure(autoconfig sdk.AutoConfigure) (*sdk.Co
 		accounts := make(sdk.ConfigAccounts)
 		for _, org := range orgs {
 			accounts[org.Name] = &sdk.ConfigAccount{
-				ID:     org.ID,
+				ID:     org.Name,
 				Type:   sdk.ConfigAccountTypeOrg,
 				Public: false,
 			}
